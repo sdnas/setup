47e5ee4c699658a96b43013fe8aeb3b4cbf90ed3
sdnas <sansd.dev@gmail.com>
2025-07-22 16:33:14 +0800

    feat: update nvm version and disable ondrej/php

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install lamp-server^ -y
 
 # Install ondrej/php
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update -y
+# sudo add-apt-repository ppa:ondrej/php -y
+# sudo apt-get update -y
 
 # Install composer 
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
@@ -20,6 +20,6 @@ sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=compose
 sudo rm /tmp/composer-setup.php
 
 # Install NodeJS
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 nvm install 20
 
